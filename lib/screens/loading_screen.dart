8fe960bc53177b67ecde6298ac5f271b79e28b8b
GauravXd <grvdhingra1999@gmail.com>
2020-04-01 05:46:36 +0530

    http

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,24 +9,50 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+    print('Triggereed InitState');
+  }
+
   void getLocation() async {
-    Position position = await Geolocator().getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.low,
-    );
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.latitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    // print(response.body);
+    // print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var Status = jsonDecode(data)['weather'][0]['main'];
+      var cityName = jsonDecode(data)['name'];
+      var currentTemp = jsonDecode(data)['main']['temp'];
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+          // child: RaisedButton(
+          //   onPressed: () {
+          //     //Get the current location
+          //     // getLocation();
+          //   },
+          //   child: Text('Get Location'),
+          // ),
+          ),
     );
   }
 }
